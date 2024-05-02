c91d452831a667fdfc9591e83b111f8fc862f8e5
Jugwan Eom <zugwan@gmail.com>
2024-05-02 06:28:24 +0000

    add support for 1.27 and 1.28 k8s bundles

--- installer/bundle_builder/ingredients/rpm/download.sh
+++ installer/bundle_builder/ingredients/rpm/download.sh
@@ -5,27 +5,21 @@
 
 set -e
 
-echo  Update the apt package index and install packages needed to use the Kubernetes apt repository
-sudo dnf install -y apt-transport-https ca-certificates curl
+sudo dnf install -y 'dnf-command(download)'
 
 echo Download containerd
 curl -LOJR https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz
 
-echo Download the Google Cloud public signing key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
-
-echo Add the Kubernetes apt repository
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+echo Add the Kubernetes repository
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/rpm/
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
 echo Update dnf package index, install kubelet, kubeadm and kubectl
-sudo dnf download {kubelet,kubeadm,kubectl}-$KUBERNETES_VERSION --arch $ARCH
-sudo dnf download kubernetes-cni-1.1.1-0 --arch $ARCH
-sudo dnf download cri-tools-1.25.0-0 --arch $ARCH
+dnf download kubelet kubeadm kubectl cri-tools kubernetes-cni --disableexcludes=kubernetes --arch ${ARCH}
